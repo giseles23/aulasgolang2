bcb5d42c9c6ecac86f86bbf464bde608ac208442
Aluno Dev25 <matheusramualdo@aluno.senai.sp>
2025-03-28 11:14:29 -0300

    repetição com especificaçõea

--- main.go
+++ main.go
@@ -6,16 +6,23 @@ import (
 
 func main(){
 	var usuario string
-	var senha int
+	var senha string
 cont := 0
 for {
 	fmt.Println("usuario: ")
 	fmt.Scan(&usuario)
 	fmt.Println("senha: ")
 	fmt.Scan(&senha)
-	if usuario != "admin" || senha != 1234 {
-		fmt.Println("senha ou usuário incorretos tente denovo")
-	} else {cont ++
+	if usuario != "admin" && senha == "1234" {
+		fmt.Println(" usuário incorreto, tente novamente")
+	}
+	if usuario == "admin" && senha != "1234" {
+		fmt.Println("senha incorreta, tente novamente")
+	}
+	if usuario != "admin" && senha != "1234" {
+		fmt.Println("usuário e senha incorretos, tente novamente")
+	}
+	if usuario == "admin" && senha == "1234" {cont ++
 		fmt.Println("pode entrar")
 	 } 
 	 if cont == 1 {
