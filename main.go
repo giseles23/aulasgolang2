a50250acdb482ea0b5e94a2e85afa15d7e8f39c5
Aluno Dev25 <matheusramualdo@aluno.senai.sp>
2025-03-28 08:40:01 -0300

    operações com entrada de dados

--- main.go
+++ main.go
@@ -1,18 +1,24 @@
-package main 
+package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main(){
-	a, b := 10, 3 
-fmt.Println("a soma é ", a + b)
-fmt.Println("a multiplicação é ", a * b)
-fmt.Println("subtrair é ", a - b)
-fmt.Println("divisão é ", a / b)
-fmt.Println("o resto da divisão é", a % b)
-
-a++
-fmt.Println("incrementar 1 ao valor de a o faz valer ", a)
-if a > 0 && b > 0 {
+	var num1 int
+	var num2 int
+fmt.Println("digite um número ")
+fmt.Scan(&num1) 
+fmt.Println("digite outro número ")
+fmt.Scan(&num2)
+fmt.Println("a soma do primeiro número com o segundo é", num1 + num2)
+fmt.Println("a multiplicação deles dois é ", num1 * num2)
+fmt.Println("a subtraçao do primeiro número com o segundo é", num1 - num2)
+fmt.Println("a divisão do número 1 pelo número 2 é ", num1 / num2)
+fmt.Println("o resto dessa divisão é ", num1 % num2)
+num1++
+fmt.Println("a incrementação no número 1 é ", num1)
+if num1 > 0 && num2 > 0 {
 	fmt.Println("números positivos")
 }
 }
