0db3bf100f95aa41cc4cc7aaa93a382318250999
Aluno Dev25 <matheusramualdo@aluno.senai.sp>
2025-03-28 08:20:20 -0300

    if

--- main.go
+++ main.go
@@ -9,4 +9,10 @@ fmt.Println("a multiplicação é ", a * b)
 fmt.Println("subtrair é ", a - b)
 fmt.Println("divisão é ", a / b)
 fmt.Println("o resto da divisão é", a % b)
+
+a++
+fmt.Println("incrementar 1 ao valor de a o faz valer ", a)
+if a > 0 && b > 0 {
+	fmt.Println("números positivos")
+}
 }
