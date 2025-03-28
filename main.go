cc12eac16a7bd20d38b1d6421e3c830415894dd5
Aluno Dev25 <matheusramualdo@aluno.senai.sp>
2025-03-28 08:55:30 -0300

    usuario e senha

--- main.go
+++ main.go
@@ -5,20 +5,15 @@ import (
 )
 
 func main(){
-	var num1 int
-	var num2 int
-fmt.Println("digite um número ")
-fmt.Scan(&num1) 
-fmt.Println("digite outro número ")
-fmt.Scan(&num2)
-fmt.Println("a soma do primeiro número com o segundo é", num1 + num2)
-fmt.Println("a multiplicação deles dois é ", num1 * num2)
-fmt.Println("a subtraçao do primeiro número com o segundo é", num1 - num2)
-fmt.Println("a divisão do número 1 pelo número 2 é ", num1 / num2)
-fmt.Println("o resto dessa divisão é ", num1 % num2)
-num1++
-fmt.Println("a incrementação no número 1 é ", num1)
-if num1 > 0 && num2 > 0 {
-	fmt.Println("números positivos")
+	var usuario string
+	var senha int
+fmt.Println("digite seu nome de usuário")
+fmt.Scan(&usuario)
+fmt.Println("digite sua senha")
+fmt.Scan(&senha)
+if usuario == "admin" && senha == 1234 {
+	fmt.Println("pode entrar")
+} else {
+	fmt.Println("acesso negado")
 }
 }
