548cb87a809f9cac4ca3de2fa48dcdf1b9c2c10f
Aluno Dev25 <matheusramualdo@aluno.senai.sp>
2025-04-03 08:20:51 -0300

    vetor

--- main.go
+++ main.go
@@ -5,29 +5,11 @@ import (
 )
 
 func main(){
-	var usuario string
-	var senha string
-cont := 0
-for {
-	fmt.Println("usuario: ")
-	fmt.Scan(&usuario)
-	fmt.Println("senha: ")
-	fmt.Scan(&senha)
-	if usuario != "admin" && senha == "1234" {
-		fmt.Println(" usuário incorreto, tente novamente")
-	}
-	if usuario == "admin" && senha != "1234" {
-		fmt.Println("senha incorreta, tente novamente")
-	}
-	if usuario != "admin" && senha != "1234" {
-		fmt.Println("usuário e senha incorretos, tente novamente")
-	}
-	if usuario == "admin" && senha == "1234" {cont ++
-		fmt.Println("pode entrar")
-	 } 
-	 if cont == 1 {
-		break
-	 }
-	}
+	var ages = [4]int{17, 16, 20, 40}
+	nomes := [4]string{"luisa mel", "fabiano", "gisele", "rafael"}
+	fmt.Println(ages)
+	fmt.Println(nomes)
+	nomes[3] = "garfield"
+	fmt.Println(nomes)
 }
 
