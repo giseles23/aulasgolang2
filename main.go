4125697d7a1f967929d88b88a82fd46c24ca8177
Aluno Dev25 <matheusramualdo@aluno.senai.sp>
2025-04-04 08:29:51 -0300

    sort, strings - funções

--- main.go
+++ main.go
@@ -1,34 +1,26 @@
 package main
 
-import (
+import ( 
 	"fmt"
-)
+"strings"
+ "sort")
 
 func main(){
-	var ages = [4]int{17, 16, 20, 40}
-	nomes := [4]string{"luisa mel", "fabiano", "gisele", "rafael"}
-	fmt.Println(ages)
-	fmt.Println(nomes)
-	nomes[3] = "garfield"
-	fmt.Println(nomes)
-	// oi fabiano
-	var score = []int{100, 200, 300, 400}
-	fmt.Println(score)
-	score[0]= 101
-	fmt.Println(score, len(score), cap(score))
-rangeOne := score[1:4]
-fmt.Println(rangeOne)
-fmt.Println(rangeOne, len(rangeOne), cap(rangeOne))
-rangeTwo := score[2:]
-fmt.Println(rangeTwo, len(rangeTwo), cap(rangeTwo))
-rangeThree := score[:3]
-fmt.Println(rangeThree, len(rangeThree), cap(rangeThree))
 
-var nomes2 = []string {"gisele2", "chico", "cachorro"}
-fmt.Println(nomes2)
-nomes2 = append(nomes2, "nome", "blabla", "klgjdsd")
-fmt.Println(nomes2, len(nomes2), cap(nomes2))
-nomes2[4] = "fascinado"
-fmt.Println(nomes2, len(nomes2), cap(nomes2))
+greeting := "hello meu amigão!"
+fmt.Println(strings.ToUpper(greeting))
+fmt.Println(strings.Contains(greeting, "amigão"))
+fmt.Println(strings.ReplaceAll(greeting, "hello", "olá"))
+fmt.Println(strings.Index(greeting, "amigão"))
+ages := []int {3, 5, 9, 1, 45}
+fmt.Println(ages)
+sort.Ints(ages)
+fmt.Println(ages)
+index := sort.SearchInts(ages, 1)
+fmt.Println(index)
+names := []string{"coraline", "terror", "fabiano"}
+sort.Strings(names)
+fmt.Println(names)
+fmt.Println(sort.SearchStrings(names, "terror"))
 }
 
