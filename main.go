f5808aafc196a4b9f1306a4f9cc64b03461ad5db
Aluno Dev25 <matheusramualdo@aluno.senai.sp>
2025-04-04 09:49:14 -0300

    vetores em for

--- main.go
+++ main.go
@@ -2,25 +2,36 @@ package main
 
 import ( 
 	"fmt"
-"strings"
- "sort")
+	"sort"
+)
 
 func main(){
-
-greeting := "hello meu amigão!"
-fmt.Println(strings.ToUpper(greeting))
-fmt.Println(strings.Contains(greeting, "amigão"))
-fmt.Println(strings.ReplaceAll(greeting, "hello", "olá"))
-fmt.Println(strings.Index(greeting, "amigão"))
-ages := []int {3, 5, 9, 1, 45}
-fmt.Println(ages)
-sort.Ints(ages)
-fmt.Println(ages)
-index := sort.SearchInts(ages, 1)
-fmt.Println(index)
-names := []string{"coraline", "terror", "fabiano"}
-sort.Strings(names)
-fmt.Println(names)
-fmt.Println(sort.SearchStrings(names, "terror"))
+	ages := []int {14, 33, 16}
+	fmt.Println(ages)
+	sort.Ints(ages)
+	fmt.Println(ages)
+	index := sort.SearchInts(ages, 1)
+	fmt.Println(index)
+	names := []string{"coraline", "terror", "fabiano"}
+	sort.Strings(names)
+	fmt.Println(names)
+	fmt.Println(sort.SearchStrings(names, "fabiano"))
+x:=3
+for x < 5 {
+fmt.Println(x)
+x++
 }
+for i:=1; i<7;  i++ {
+fmt.Println("segundos ", i)
+}
+for i:=0; i <len(names); i++{
+fmt.Println(names [i])
+}
+
+for index, value := range names {
+	fmt.Println("o indice é ", index,"e o nome é ", value)
+}
+
+}
+	
 
