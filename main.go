aaed21ad84de016b0fdec5a26b20d98552e125fd
Aluno Dev25 <matheusramualdo@aluno.senai.sp>
2025-04-10 10:19:02 -0300

    exercicio if idade

--- main.go
+++ main.go
@@ -5,29 +5,15 @@ import (
 )
 
 func main(){ 
-age:= 45
-fmt.Println(age <= 50)
-fmt.Println(age >= 50)
-fmt.Println(age == 50)
-fmt.Println(age != 50)
-
-if age < 30 {
-    fmt.Println("menor que 30 anos")
-} else if age <40 { 
-    fmt.Println("menor que 40 anos")
-} else { fmt.Println("não é menor que 40")
-}
-names := []string {"fabiano", "batman", "gisele", "cascão", "holmes", "bem 10"}
-for index, valor:= range names {
-    if index == 1 {
-        fmt.Println("conntinua após a posição", index,  "e nome", valor)
-    continue
-    }
-    if index > 2 {
-        fmt.Println("sair antes de", valor)
-        break
-    }
-fmt.Println("nome:", valor)
+    var idade int
+fmt.Println("qual sua idade?")
+fmt.Scan(&idade)
+if idade <18 {
+    fmt.Println("voce é menor de idade")
+} else if idade >=18 && idade <= 60 { 
+    fmt.Println("você é adulto")
+} else {
+fmt.Println("voce é idoso")
 }
 }
 
