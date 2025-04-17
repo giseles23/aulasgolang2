6bbfd45f256fcd1657a1ef128abb6963ee9cb2e6
Aluno Dev25 <matheusramualdo@aluno.senai.sp>
2025-04-17 10:45:43 -0300

    map

--- main.go
+++ main.go
@@ -3,20 +3,12 @@ package main
  import (
     "fmt")
 
-func dadosPessoa(idade int) (int, string){
-var condicao string
-if idade >= 18 {
-    condicao = "você é maior de idade"
-} else {
-    condicao = "você é menor de idade"
-}
-return idade, condicao
-}
 
 func main(){
-    var idadeusuario int
-fmt.Println("qual sua idade?")
-fmt.Scan(&idadeusuario)
-idade, condicao := dadosPessoa(idadeusuario)
-fmt.Println("você tem", idade, "anos e", condicao)
+    alunoidade := make(map[string]int)
+	alunoidade ["fabiano"] = 16
+	alunoidade ["gisele"] = 18
+	alunoidade ["abobora"] = 3
+	alunoidade ["xerequinha"] = 15
+	fmt.Println("idade da xana", alunoidade["xerequinha"])
 }
