507a432b46c84023e79221be980c2b262a8f2cbb
Aluno Dev25 <matheusramualdo@aluno.senai.sp>
2025-04-17 11:08:04 -0300

    novo comit aual 17 de abril

--- main.go
+++ main.go
@@ -11,4 +11,13 @@ func main(){
 	alunoidade ["abobora"] = 3
 	alunoidade ["xerequinha"] = 15
 	fmt.Println("idade da xana", alunoidade["xerequinha"])
+	notasalunos := map[string]float64{
+		"fabiano" : 7.9,
+		"abobora" : 10,
+		"xerequinha" : 3.8,
+		"gisele" : 10,
+	} 
+	for nome, nota := range notasalunos{
+fmt.Printf("%s tirou a nota %.1f\n", nome, nota )
+	}
 }
