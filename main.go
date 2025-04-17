591905722a631a4ca89ebd794b0878af61d735c3
Aluno Dev25 <matheusramualdo@aluno.senai.sp>
2025-04-17 08:38:48 -0300

    retornar dois tipos numa funcao

--- main.go
+++ main.go
@@ -1,69 +1,32 @@
 package main
 
-import
- ( 
-	"fmt"
+import (
+	"fmt"	
 )
 
-var opcao int
-var saldo float64
-var opcao2 int
-func Inicio(){
-    fmt.Println("Digite seu saldo")
-    fmt.Scan(&saldo)
+func dividir(dividendo int, divisor int) (int, string){
+if divisor == 0 {
+	 return 0, "erro na divisão por zero"
 }
-func Opcoes(){
+return dividendo/divisor, "sem erro"
+}
+func operacaoBasica(a int, b int) (int, int, int){
+soma := a + b
+multiplicacao := a * b
+subtracao := a - b
+return soma, multiplicacao, subtracao
 
-    fmt.Println("escolha sua opção: 'sacar'(1) ou 'depositar'(2) - 'ver saldo'(3)")
-    fmt.Scan(&opcao)
 }
-func Sacar() {
-    var valor float64
-   if opcao == 1 {
-fmt.Println("digite quanto você quer sacar")
-fmt.Scan(&valor)
-   
-if valor <= saldo {
-    saldo = saldo - valor
-    fmt.Printf("saldo suficiente, saldo atual é %.2f\n", saldo)
-} else { fmt.Println("saldo insuficiente")
-}
-}
-}
-func Depositar(){
-    var valor float64
-    if opcao == 2 {
- fmt.Println("digite quanto você quer depositar")
- fmt.Scan(&valor)
- saldo = valor + saldo
- fmt.Printf("seu saldo atual é %.2f\n", saldo)
- 
-    }
-}
-func VerSaldo(){
-    if opcao == 3 {
-        fmt.Printf("seu saldo atual é %.2f\n", saldo)
-
-    }
+func main() {
+resultado, erro := dividir(10,2)
+if erro != "sem erro" {
+	fmt.Println(erro)
+} else {
+	fmt.Println("O resultado da divisão é:", resultado)
 }
 
-func SairOUContinuar(){
-for {
-    Opcoes()
-Sacar()
-Depositar()
-VerSaldo()
-fmt.Println("'sair'(1) 'continuar'(2)")
-fmt.Scan(&opcao2)
-if opcao2 == 1 {
-    break
-} else if opcao2 == 2 {
-    continue
-}
-}
-}
-
-func main(){ 
-    Inicio()
-    SairOUContinuar()
+soma, mult, sub := operacaoBasica(10,2)
+fmt.Println(soma)
+fmt.Println(mult)
+fmt.Println(sub)
 }
